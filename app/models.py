e0034b748e10f75ad7e9c0b6479ccacd5e642b5e
Eric Schles <ericschles@gmail.com>
2015-12-09 15:22:19 -0500

    updating

--- app/models.py
+++ app/models.py
@@ -4,8 +4,12 @@ class Data(db.Model):
     __tablename__ = 'data'
     id = db.Column(db.Integer,primary_key=True)
     date_created = db.Column(db.DateTime)
+    name = db.Column(db.String(400))
+    email = db.Column(db.String(500))
     
-    def __init__(self,date_created):
+    def __init__(self,date_created,name,email):
         self.date_created = date_created
+        self.name = name
+        self.email = email
     def __repr__(self):
-        return "<Data %r>" % str(self.date_created)
+        return "<Data %r>" % str(self.name)
--- app/schema.sql
+++ app/schema.sql
@@ -1,5 +1,7 @@
 drop table if exists data;
     create table data(
       id integer primary key autoincrement,
-      date_created text not null
+      date_created text not null,
+      name text not null,
+      email text not null
 );      	   
